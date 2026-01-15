3f9336da3be4bb48348fc33a4c92196955cc75ad
Dzarek <jarekjanas95@gmail.com>
2026-01-15 22:41:58 +0100

    c

--- app/card/[id]/page.tsx
+++ app/card/[id]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useParams, useRouter } from "next/navigation";
-import PlayScreen from "../components/PlayScreen";
+import PlayScreen from "../../components/PlayScreen";
 
 export default function CardPage() {
   const { id } = useParams<{ id: string }>();
