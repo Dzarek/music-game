5806fa6b500bba834174206a64df0dd669299a33
Dzarek <jarekjanas95@gmail.com>
2026-02-05 21:06:41 +0100

    test

--- app/card/[id]/page.tsx
+++ app/card/[id]/page.tsx
@@ -1,17 +1,15 @@
 // "use client";
 
-import { useParams, useRouter } from "next/navigation";
-import PlayScreen from "../../components/PlayScreen";
+import CardComponent from "@/app/components/CardId";
+
+// import { useParams, useRouter } from "next/navigation";
+// import PlayScreen from "../../components/PlayScreen";
 
 export default function CardPage() {
-  const { id } = useParams<{ id: string }>();
-  const router = useRouter();
+  // const { id } = useParams<{ id: string }>();
+  // const router = useRouter();
 
-  if (!id) return null;
+  // if (!id) return null;
 
-  return (
-    <div className="h-dvh w-vw overflow-hidden">
-      <PlayScreen cardId={id} onNext={() => router.push("/?autostart=true")} />
-    </div>
-  );
+  return <CardComponent />;
 }
