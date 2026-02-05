7dd381cf3e19c57a2bb1e8079fe1a083b0c9fb20
Dzarek <jarekjanas95@gmail.com>
2026-02-05 20:37:43 +0100

    test2

--- app/play/page.tsx
+++ app/play/page.tsx
@@ -1,21 +1,10 @@
-"use client";
-
-import { useSearchParams, useRouter } from "next/navigation";
-import PlayScreen2 from "../components/PlayScreen2";
+import { Suspense } from "react";
+import PlayClient from "./PlayClient";
 
 export default function PlayPage() {
-  const params = useSearchParams();
-  const router = useRouter();
-
-  const cardId = params.get("card");
-  if (!cardId) return null;
-
   return (
-    <div className="h-dvh w-vw overflow-hidden bg-black">
-      <PlayScreen2
-        cardId={cardId}
-        onNext={() => router.replace("/scan?autostart=true")}
-      />
-    </div>
+    <Suspense fallback={null}>
+      <PlayClient />
+    </Suspense>
   );
 }
