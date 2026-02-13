b251b43945cb2b87235a7af1788a9a67f7ec5920
Dzarek <jarekjanas95@gmail.com>
2026-02-13 21:46:22 +0100

    error 500

--- app/api/auth/spotify/callback/route.ts
+++ app/api/auth/spotify/callback/route.ts
@@ -3,6 +3,10 @@ import { NextResponse } from "next/server";
 export async function GET(req: Request) {
   const url = new URL(req.url);
   const code = url.searchParams.get("code");
+  console.log("Spotify code:", code);
+  console.log("Client ID:", process.env.SPOTIFY_CLIENT_ID);
+  console.log("Client Secret:", process.env.SPOTIFY_CLIENT_SECRET);
+  console.log("Redirect URI:", process.env.SPOTIFY_REDIRECT_URI);
   if (!code) return NextResponse.json({ error: "No code" }, { status: 400 });
 
   const tokenRes = await fetch("https://accounts.spotify.com/api/token", {
@@ -25,7 +29,7 @@ export async function GET(req: Request) {
   });
 
   const data = await tokenRes.json();
-
+  console.log("Token response:", data);
   const res = NextResponse.redirect("/"); // wróć do gry
   res.cookies.set("spotify_access_token", data.access_token, {
     httpOnly: true,
