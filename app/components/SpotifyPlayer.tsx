301c850d173a702e9d4b25d16464a250d68c9dff
Dzarek <jarekjanas95@gmail.com>
2026-02-26 18:22:47 +0100

    e

--- app/components/SpotifyPlayer.tsx
+++ app/components/SpotifyPlayer.tsx
@@ -69,13 +69,13 @@ export default function SpotifyPlayer({ cardId, onNext }: Props) {
         if (!spotifyTrackId) throw new Error("no spotifyTrackId");
 
         /* ---------- SDK LOAD ---------- */
-        // if (!document.getElementById("spotify-sdk")) {
-        //   const script = document.createElement("script");
-        //   script.id = "spotify-sdk";
-        //   script.src = "https://sdk.scdn.co/spotify-player.js";
-        //   script.async = true;
-        //   document.body.appendChild(script);
-        // }
+        if (!document.getElementById("spotify-sdk")) {
+          const script = document.createElement("script");
+          script.id = "spotify-sdk";
+          script.src = "https://sdk.scdn.co/spotify-player.js";
+          script.async = true;
+          document.body.appendChild(script);
+        }
 
         window.onSpotifyWebPlaybackSDKReady = () => {
           if (destroyed) return;
